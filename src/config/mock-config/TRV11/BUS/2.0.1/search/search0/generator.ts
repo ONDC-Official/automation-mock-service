e9a16c73314ac54605a90e8c08313310a7c01925
shivang1131 <57249768+shivang1131@users.noreply.github.com>
2025-05-16 12:50:53 +0530

    Update generator.ts

--- src/config/mock-config/TRV11/BUS/2.0.1/search/search0/generator.ts
+++ src/config/mock-config/TRV11/BUS/2.0.1/search/search0/generator.ts
@@ -4,5 +4,7 @@ export async function search0Generator(
   existingPayload: any,
   sessionData: SessionData
 ) { 
+  delete existingPayload.context.bpp_id
+  delete existingPayload.context.bpp_uri
   return existingPayload;
 }
