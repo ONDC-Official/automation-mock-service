ebb4be5487ac2e4b7b62fb93b646c6c9bcf56a90
Shivang Dudani <shivangdudani113@gmail.com>
2025-05-29 13:23:30 +0530

    changing API_SERVICE_LAYER to API_SERVICE_URL

--- src/utils/request-utils.ts
+++ src/utils/request-utils.ts
@@ -13,7 +13,7 @@ export async function sendToApiService(
 	try {
 		const domain = process.env.DOMAIN;
 		const version = body.context.version ?? body.context.core_version;
-		const url = `${process.env.API_SERVICE_LAYER}/${domain}/${version}/mock/${action}`;
+		const url = `${process.env.API_SERVICE_URL}/${domain}/${version}/mock/${action}`;
 		const subscriber_url = queryData.subscriber_url;
     	if (!subscriber_url) {
       logger.error("subscriber url not provided");
@@ -32,9 +32,9 @@ export async function sendToApiService(
 }
 
 export function createSellerUrl(domain: string, version: string) {
-	return `${process.env.API_SERVICE_LAYER}/${domain}/${version}/seller`;
+	return `${process.env.API_SERVICE_URL}/${domain}/${version}/seller`;
 }
 
 export function createBuyerUrl(domain: string, version: string) {
-	return `${process.env.API_SERVICE_LAYER}/${domain}/${version}/buyer`;
+	return `${process.env.API_SERVICE_URL}/${domain}/${version}/buyer`;
 }
