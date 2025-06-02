2e6d54abe6eeab29c9fb8e64b43d4bce76f82343
Shivang Dudani <shivangdudani113@gmail.com>
2025-06-02 13:51:35 +0530

    fixing created_at being an empty string issue

--- src/config/mock-config/TRV11/METRO/2.0.0/on_confirm/on_confirm_delayed/generator.ts
+++ src/config/mock-config/TRV11/METRO/2.0.0/on_confirm/on_confirm_delayed/generator.ts
@@ -111,7 +111,7 @@ export async function onConfirmDelayedGenerator(existingPayload: any,sessionData
   console.log("the delay duration is", delay_duration)
   await delay(delay_duration*1000);
   const now = new Date().toISOString();
-  existingPayload.message.order.created_at = sessionData.created_at
+  existingPayload.message.order.created_at = now
   existingPayload.message.order.updated_at = now
   return existingPayload;
 }
