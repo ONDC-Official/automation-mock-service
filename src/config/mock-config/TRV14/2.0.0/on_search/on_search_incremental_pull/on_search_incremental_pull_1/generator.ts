6540c15302f3c7926eafe0f38d78b3ce8a829100
Mohd Ahsan <63741546+mofahsan@users.noreply.github.com>
2025-07-22 22:33:27 +0530

    Feat add incremental pull flow

--- src/config/mock-config/TRV14/2.0.0/on_search/on_search_incremental_pull/on_search_incremental_pull_1/generator.ts
+++ src/config/mock-config/TRV14/2.0.0/on_search/on_search_incremental_pull/on_search_incremental_pull_1/generator.ts
@@ -9,33 +9,35 @@ function getRandomDateBetween(start: any, end: any) {
 }
 
 export async function onSearchIncrementalPull1Generator(existingPayload: any, sessionData: any) {
-  const provider = existingPayload.message.catalog.providers[0];
-  // Set collected_by
-  if (sessionData.collected_by && provider.payments && provider.payments[0]) {
-    provider.payments[0].collected_by = sessionData.collected_by;
-  }
-  // Set tags
-  if (sessionData.tags) {
-    existingPayload.message.catalog.tags = sessionData.tags;
-  }
-  // Set timerange
-  if (sessionData.start_time && sessionData.end_time) {
-    const randomDate = getRandomDateBetween(sessionData.start_time, sessionData.end_time);
-    provider.time = {
-      range: {
-        start: randomDate.hour(5).minute(30).second(0).format('YYYY-MM-DDTHH:mm:ss.SSS[Z]'),
-        end: randomDate.hour(23).minute(0).second(0).format('YYYY-MM-DDTHH:mm:ss.SSS[Z]')
-      }
+  
+  // Set payment_collected_by if present
+  if (sessionData.collected_by && existingPayload.message?.catalog?.providers?.[0]?.payments?.[0]) {
+    existingPayload.message.catalog.providers[0].payments[0].collected_by = sessionData.collected_by;
+}
+
+// Set tags if present // wrong in developer guide bap_terms shouldn't be in on_search
+// if (sessionData.tags && existingPayload.message?.catalog) {
+//     existingPayload.message.catalog.tags = [...existingPayload.message.catalog.tags, ...sessionData.tags[0]]
+// }
+
+// Set timerange
+if (sessionData.start_time && sessionData.end_time) {
+    // const randomDate = getRandomDateBetween(sessionData.start_time, sessionData.end_time);
+    existingPayload.message.catalog.providers[0].time = {
+        range: {
+            // start: randomDate.hour(5).minute(30).second(0).format('YYYY-MM-DDTHH:mm:ss.SSS[Z]'),
+            // end: randomDate.hour(23).minute(0).second(0).format('YYYY-MM-DDTHH:mm:ss.SSS[Z]')
+            start: sessionData.start_time,
+            end: sessionData.end_time
+        }
     };
-  }
-  // Update items if present in session
-  if (Array.isArray(sessionData.items)) {
-    provider.items = sessionData.items.map((item: any) => {
-      if (provider.time) {
-        item.time = { ...item.time, ...provider.time };
-      }
-      return item;
-    });
-  }
-  return existingPayload;
+}
+
+// NOTE: sessionData.items is NOT used here because page 1 contains no items
+// Items catalog starts from on_search_seller_pagination_2 and on_search_seller_pagination_3
+
+return existingPayload;
 } 
+
+
+
