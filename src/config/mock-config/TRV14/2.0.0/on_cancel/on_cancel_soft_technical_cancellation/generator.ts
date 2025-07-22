5198e24035aa8cae357d1fa0394781f35087eb9f
Mohd Ahsan <63741546+mofahsan@users.noreply.github.com>
2025-07-22 23:23:16 +0530

    feat: add technical cancellation

--- src/config/mock-config/TRV14/2.0.0/on_cancel/on_cancel_soft_technical_cancellation/generator.ts
+++ src/config/mock-config/TRV14/2.0.0/on_cancel/on_cancel_soft_technical_cancellation/generator.ts
@@ -1,3 +1,8 @@
 export async function onCancelSoftTechnicalCancellationGenerator(existingPayload: any, sessionData: any) {
-
+  if (sessionData.order_id) {
+    existingPayload.message.order_id = sessionData.order_id;
+  }
+  if(sessionData.cancellation_reason_id){
+    existingPayload.message.cancellation_reason_id = sessionData.cancellation_reason_id;
+  }
   return existingPayload;} 
