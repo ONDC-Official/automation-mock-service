fba765cc0b3f241221fc6634fc676270e68c6585
Mohd Ahsan <63741546+mofahsan@users.noreply.github.com>
2025-08-19 18:47:14 +0530

    feat form flow

--- src/config/mock-config/TRV14/2.0.0/on_select/generator.ts
+++ src/config/mock-config/TRV14/2.0.0/on_select/generator.ts
@@ -220,7 +220,7 @@ export async function onSelectDefaultGenerator(existingPayload: any, sessionData
             "form": {
                 "id": "F01",
                 "mime_type": "text/html",
-                "url": `http://${process.env.FORM_SERVICE}/forms/${sessionData.domain}/additional-details-form?session_id=${sessionData.session_id}&flow_id=${sessionData.flow_id}&transaction_id=${existingPayload.context.transaction_id}`,
+                "url": `${process.env.FORM_SERVICE}/forms/${sessionData.domain}/additional-details-form?session_id=${sessionData.session_id}&flow_id=${sessionData.flow_id}&transaction_id=${existingPayload.context.transaction_id}`,
                 "resubmit": false,
                 "multiple_sumbissions": false
             },
