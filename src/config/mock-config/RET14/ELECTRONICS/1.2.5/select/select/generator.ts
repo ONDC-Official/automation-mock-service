dbe1a13df6754062c91d66a29960ad8c476b21ea
Sahil Sharma <sahil-ondc@users.noreply.github.com>
2025-08-19 17:46:56 +0530

    fix: select call

--- src/config/mock-config/RET14/ELECTRONICS/1.2.5/select/select/generator.ts
+++ src/config/mock-config/RET14/ELECTRONICS/1.2.5/select/select/generator.ts
@@ -3,7 +3,7 @@ import { stateCodes } from "../state-codes";
 
 type SelectInputType = {
 	provider?: string;
-	provider_location?: any;
+	provider_location?: string[];
 	location_gps?: string;
 	location_pin_code?: string;
 	items?: {
@@ -25,14 +25,16 @@ export async function select_generator(
   if (inputs.provider) {
 		existingPayload.message.order.provider.id = inputs.provider;
 	}
+	
 	if (inputs.provider_location) {
-		const providerLocations = (inputs.provider_location || "")
-			.split(",")
-			.map((location: string) => location.trim())
-			.filter(Boolean)
-			.map((location: string) => ({ id: location }));
-		existingPayload.message.order.provider.locations = providerLocations
+		existingPayload.message.order.provider.locations =
+			inputs.provider_location.map((location) => {
+				return {
+					id: location,
+				};
+			});
 	}
+	
 	if (inputs.location_gps) {
 		existingPayload.message.order.fulfillments[0].end.location.gps =
 			inputs.location_gps;
