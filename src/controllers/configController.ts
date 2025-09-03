28e916df302c192fd4222d9c242db0d4c2b1792b
extedcouD <extedcoud@gmail.com>
2025-09-03 12:21:54 +0530

    changes

--- src/controllers/configController.ts
+++ src/controllers/configController.ts
@@ -1,16 +1,24 @@
 import { Request, Response } from "express";
-import { getAllMockActionIds } from "../config/mock-config";
+import { actionConfig } from "../config/mock-config";
 import { getMockAction } from "../config/mock-config/TRV14/action-factory";
 import logger from "@ondc/automation-logger";
 export const getMockConfigController = (req: Request, res: Response) => {
 	try {
 		logger.info("Fetching mock configs");
-		const allActionsIds = getAllMockActionIds();
-		const completeConfig: any = {};
-		allActionsIds.forEach((actionId) => {
+		const completeConfig: any[] = [];
+		const actionFactory = actionConfig.codes;
+		for (const actionObj of actionFactory) {
+			const actionId = actionObj.action_id;
+			const code = actionObj.code;
+			const action = actionObj.action;
 			const mockAction = getMockAction(actionId);
-			completeConfig[actionId] = mockAction.mockActionConfig;
-		});
+			completeConfig.push({
+				action_id: actionId,
+				actionCode: code,
+				action: action,
+				config: mockAction.mockActionConfig,
+			});
+		}
 		res.json(completeConfig);
 	} catch (e) {
 		logger.error("Error getting mock config", {}, e);
