2eb767c5d7d7d5ae55ecffdba65e154176196ea4
Mohd Ahsan <63741546+mofahsan@users.noreply.github.com>
2025-09-03 09:56:31 +0530

    fis 13 cleanup

--- src/config/mock-config/TRV14/2.0.0/on_cancel/on_cancel_confirm_technical_cancellation/generator.ts
+++
@@ -1,9 +0,0 @@
-export async function onCancelConfirmTechnicalCancellationGenerator(existingPayload: any, sessionData: any) {
-
-  if(sessionData.order){
-    existingPayload.message.order = sessionData.order;
-  }
-  existingPayload.message.order.status = "CANCELLED";
-
-  return existingPayload;
-} 
--- src/types/type-utils.ts
+++
@@ -1,4 +0,0 @@
-export function isArrayKey<T>(key: keyof T, obj: T): boolean {
-	// Check if the key exists and the value is an array
-	return Array.isArray(obj[key]);
-}
