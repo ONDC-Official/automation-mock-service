60c576690d80680b6458cf56573a0afd9e66aedc
extedcouD <extedcoud@gmail.com>
2025-10-31 12:01:28 +0530

    feat: enhance fulfillment timing and update session data in mock configurations

--- src/config/mock-config/RET10/GROCERY/1.2.5/on_select/on_select_slotted_delivery/generator.ts
+++ src/config/mock-config/RET10/GROCERY/1.2.5/on_select/on_select_slotted_delivery/generator.ts
@@ -1,3 +1,4 @@
+import { start } from "repl";
 import { SessionData } from "../../../../session-types";
 import { createQuote } from "../../api-objects/breakup";
 import { RET10GROCERY125Catalog } from "../../on_search/catalog";
@@ -151,5 +152,23 @@ export async function on_select_slotted_delivery_generator(
 		quote.breakup.push(fulfillment_breakup[i]);
 	}
 	existingPayload.message.order.quote = quote;
+
+	// todays date +2 hours for index 0 then add 2 hours for each subsequent index
+	const curr = new Date();
+	const baseTime = new Date(curr.getTime() + 2 * 60 * 60 * 1000);
+	for (let i = 0; i < existingPayload.message.order.fulfillments.length; i++) {
+		const start = new Date(baseTime.getTime() + i * 2 * 60 * 60 * 1000);
+		const end = new Date(start.getTime() + 1 * 60 * 60 * 1000);
+		existingPayload.message.order.fulfillments[i].end = {
+			time: {
+				range: {
+					start: start.toISOString(),
+					end: end.toISOString(),
+				},
+			},
+		};
+		delete existingPayload.message.order.fulfillments[i]["@ondc/org/TAT"];
+	}
+
 	return existingPayload;
 }
