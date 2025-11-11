ddf850658a8bc58c7b5e70fbb3e75b2454e15901
Sharique <sharique@WILASSETA0046.local>
2025-11-11 18:55:38 +0530

    fix: igm issue

--- src/config/mock-config/RET13/BPC/1.2.5/generation-pipeline.ts
+++ src/config/mock-config/RET13/BPC/1.2.5/generation-pipeline.ts
@@ -51,7 +51,8 @@ function yamlToJson(filePath: string): object {
 
 export async function createMockResponseRET13_125(
   actionID: string,
-  sessionData: SessionData
+  sessionData: SessionData,
+  inputs?: Record<string, string>
 ) {
   const factoryData = loadFactoryYaml(
     path.resolve(__dirname, "../../factory.yaml")
@@ -92,5 +93,5 @@ export async function createMockResponseRET13_125(
     logger.info(`L2 error found: ${JSON.stringify(error_message)}`);
     return payload;
   }
-  return await Generator(actionID, payload, sessionData);
+  return await Generator(actionID, payload, sessionData, inputs);
 }
