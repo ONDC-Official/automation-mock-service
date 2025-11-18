35c3364bbb3d1b7e1802bff1a66f9f85bbc8c517
Sahil Sharma <sahil-ondc@users.noreply.github.com>
2025-11-18 17:39:18 +0530

    feature:(added buyer side and seller side canellation flow)

--- src/config/mock-config/NIC2004:60232/LOGISTICS/1.2.5/cancel/generator.ts
+++ src/config/mock-config/NIC2004:60232/LOGISTICS/1.2.5/cancel/generator.ts
@@ -1,3 +1,12 @@
-export const cancelGenerator = (existingPayload: any, sessionData: any) => {
+import { SessionData, Input } from "../../../session-types";
+
+export const cancelGenerator = (existingPayload: any,
+  sessionData: SessionData,
+  inputs: Input | undefined,
+  action_id:string
+) => {
+  existingPayload.message.order_id = sessionData.order_id
+  existingPayload.message.cancellation_reason_id = inputs?.cancellation_reason_id || "126"
+
   return existingPayload;
 };
