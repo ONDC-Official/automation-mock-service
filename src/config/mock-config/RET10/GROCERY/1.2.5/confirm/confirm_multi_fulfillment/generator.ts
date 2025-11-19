93b0934c60d5d304cef9d6462d3ac3459e7b9472
Sahil Sharma <sahil-ondc@users.noreply.github.com>
2025-11-19 17:04:58 +0530

    fix: removed on_status updation from generator

--- src/config/mock-config/RET10/GROCERY/1.2.5/confirm/confirm_multi_fulfillment/generator.ts
+++ src/config/mock-config/RET10/GROCERY/1.2.5/confirm/confirm_multi_fulfillment/generator.ts
@@ -1,5 +1,5 @@
 import { SessionData } from "../../../../session-types";
-import { updateFlowConfigWithDynOnStatuses } from "../../on_init/on_init_multi_fulfillment/generator";
+// import { updateFlowConfigWithDynOnStatuses } from "../../on_init/on_init_multi_fulfillment/generator";
 import { confirm_generator } from "../confirm/generator";
 
 export async function confirm_multi_fulfillment_generator(
@@ -7,9 +7,9 @@ export async function confirm_multi_fulfillment_generator(
 	sessionData: SessionData
 ) {
 	const payload = await confirm_generator(existingPayload, sessionData);
-	await updateFlowConfigWithDynOnStatuses(
-		payload.message.order.fulfillments,
-		sessionData
-	);
+	// await updateFlowConfigWithDynOnStatuses(
+	// 	payload.message.order.fulfillments,
+	// 	sessionData
+	// );
 	return payload;
 }
