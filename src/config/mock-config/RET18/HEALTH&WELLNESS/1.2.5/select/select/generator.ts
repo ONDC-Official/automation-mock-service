f739057ce6a84e7a6acc7fc8f325f2909efa8e18
Sahil Sharma <sahil-ondc@users.noreply.github.com>
2025-12-08 19:38:40 +0530

    fix: provider id in select

--- src/config/mock-config/RET18/HEALTH&WELLNESS/1.2.5/select/select/generator.ts
+++ src/config/mock-config/RET18/HEALTH&WELLNESS/1.2.5/select/select/generator.ts
@@ -21,6 +21,9 @@ export async function select_generator(
 
   const inputs = sessionData.user_inputs as SelectInputType;
 	if (!inputs) return existingPayload;
+	if(inputs.provider){
+		existingPayload.message.order.provider.id = inputs.provider
+	}
 
   if (inputs.provider_location) {
 		existingPayload.message.order.provider.locations =
