923e9becfb7131b7518465b12fbad4e8cf4d9b61
Sahil Sharma <sahil-ondc@users.noreply.github.com>
2025-12-17 13:49:13 +0530

    fix: tags in on_status of finance flow

--- src/config/mock-config/RET14/ELECTRONICS/1.2.5/on_status/on_status_pending_fin/generator.ts
+++ src/config/mock-config/RET14/ELECTRONICS/1.2.5/on_status/on_status_pending_fin/generator.ts
@@ -23,7 +23,31 @@ export async function on_status_pending_fin_generator(
   }
   generalPayload.message.order.quote = sessionData.quote;
   generalPayload.message.order.payment = sessionData.payment;
-  generalPayload.message.order.tags = sessionData.order_tags;
+  generalPayload.message.order.tags = (sessionData.order_tags || []).map(
+    (tag: any) => {
+      // remove accept_bap_terms from bpp_terms
+      if (tag.code === "bpp_terms") {
+        return {
+          ...tag,
+          list: tag.list.filter(
+            (item: any) => item.code !== "accept_bap_terms"
+          ),
+        };
+      }
+
+      // remove static_terms from bap_terms
+      if (tag.code === "bap_terms") {
+        return {
+          ...tag,
+          list: tag.list.filter(
+            (item: any) => item.code !== "static_terms"
+          ),
+        };
+      }
+
+      return tag;
+    }
+  );
 
   return generalPayload;
 }
