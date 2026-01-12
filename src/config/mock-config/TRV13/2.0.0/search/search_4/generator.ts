c5a7a78001aa7ccea6209e7e528cc5f5fe2f911b
Sharique <sharique@WILASSETA0046.local>
2026-01-12 17:34:08 +0530

    fix: select provider details from ui

--- src/config/mock-config/TRV13/2.0.0/search/search_4/generator.ts
+++ src/config/mock-config/TRV13/2.0.0/search/search_4/generator.ts
@@ -6,6 +6,23 @@ export async function search_specific_hotel_generator(
 ) {
   delete existingPayload.context.bpp_uri;
   delete existingPayload.context.bpp_id;
+  const userInput =
+    typeof sessionData?.user_inputs?.data === "string"
+      ? JSON.parse(sessionData?.user_inputs?.data)
+      : sessionData?.user_inputs?.data;
+  existingPayload.message.intent.provider = {
+    id: userInput?.providerId ?? "ondc-hotel-provider1",
+    descriptor: {
+      name: userInput?.providerName ?? "hotel-abc",
+    },
+    time: {
+      label: "AVAILABLE",
+      range: {
+        start: userInput?.checkInDate ?? "2026-01-15T00:00:00Z",
+        end: userInput?.checkOutDate ?? "2026-01-16T00:00:00Z",
+      },
+    },
+  };
   // const currentDate = new Date();
   // const futureDate = new Date(currentDate);
   // futureDate.setDate(currentDate.getDate() + 5);
