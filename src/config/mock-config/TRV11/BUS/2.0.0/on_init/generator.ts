0bd476e73006e66fb84eb92e19bb1462d6c10682
Shivang Dudani <shivangdudani113@gmail.com>
2026-02-09 15:23:28 +0530

    fixing common issues

--- src/config/mock-config/TRV11/BUS/2.0.0/on_init/generator.ts
+++ src/config/mock-config/TRV11/BUS/2.0.0/on_init/generator.ts
@@ -1,102 +1,31 @@
 import { SessionData } from "../../../session-types";
 
+const generateRandomId = () => {
+	return Math.random().toString(36).substring(2, 15);
+  };
+const transformPayments = (payments:any) => {
+return payments.map((payment:any) => {
+	return {
+		id: generateRandomId(),
+		collected_by: payment.collected_by,
+		status: "NOT-PAID",
+		type: "PRE-ORDER",
+		params: {
+			bank_code: "XXXXXXXX",
+			bank_account_number: "xxxxxxxxxxxxxx",
+		},
+		tags: payment.tags,
+		};
+	});
+};
+
 export async function onInitGenerator(
   existingPayload: any,
   sessionData: SessionData
 ) {
-  const randomId = Math.random().toString(36).substring(2, 15);
 
-  const payments = [
-    {
-      id: randomId,
-      collected_by: "BAP",
-      status: "NOT-PAID",
-      type: "PRE-ORDER",
-      params: {
-        bank_code: "XXXXXXXX",
-        bank_account_number: "xxxxxxxxxxxxxx",
-      },
-      tags: [
-        {
-          descriptor: {
-            code: "BUYER_FINDER_FEES",
-          },
-          display: false,
-          list: [
-            {
-              descriptor: {
-                code: "BUYER_FINDER_FEES_PERCENTAGE",
-              },
-              value: "1",
-            },
-            {
-              descriptor: {
-                code: "BUYER_FINDER_FEES_TYPE",
-              },
-              value: "percent",
-            },
-          ],
-        },
-        {
-          descriptor: {
-            code: "SETTLEMENT_TERMS",
-          },
-          display: false,
-          list: [
-            {
-              descriptor: {
-                code: "SETTLEMENT_WINDOW",
-              },
-              value: "PT60M",
-            },
-            {
-              descriptor: {
-                code: "SETTLEMENT_BASIS",
-              },
-              value: "Delivery",
-            },
-            {
-              descriptor: {
-                code: "SETTLEMENT_TYPE",
-              },
-              value: "neft",
-            },
-            {
-              descriptor: {
-                code: "MANDATORY_ARBITRATION",
-              },
-              value: "true",
-            },
-            {
-              descriptor: {
-                code: "COURT_JURISDICTION",
-              },
-              value: "New Delhi",
-            },
-            {
-              descriptor: {
-                code: "DELAY_INTEREST",
-              },
-              value: "2.5",
-            },
-            {
-              descriptor: {
-                code: "STATIC_TERMS",
-              },
-              value: "https://www.abc.com/settlement-terms/",
-            },
-            {
-              descriptor: {
-                code: "SETTLEMENT_AMOUNT",
-              },
-              value: "70",
-            },
-          ],
-        },
-      ],
-    },
-  ];
-  existingPayload.message.order.payments = payments;
+const payments = transformPayments(sessionData.payments)
+	existingPayload.message.order.payments = payments;
   if (sessionData.items.length > 0) {
     existingPayload.message.order.items = sessionData.items;
   }
