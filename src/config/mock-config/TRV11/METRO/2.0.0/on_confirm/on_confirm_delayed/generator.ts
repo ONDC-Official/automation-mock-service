cbcbf414d35bf9535984d96f5b029ee46313d722
Shivang Dudani <shivangdudani113@gmail.com>
2026-02-20 17:06:03 +0530

    adding display in tags

--- src/config/mock-config/TRV11/METRO/2.0.0/on_confirm/on_confirm_delayed/generator.ts
+++ src/config/mock-config/TRV11/METRO/2.0.0/on_confirm/on_confirm_delayed/generator.ts
@@ -64,6 +64,7 @@ function updateFulfillmentsWithParentInfo(fulfillments: any[]): void {
       descriptor: {
         code: "TICKET_INFO",
       },
+      display: false,
       list: [
         {
           descriptor: {
