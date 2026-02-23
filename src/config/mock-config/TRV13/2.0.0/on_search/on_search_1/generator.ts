59936f7da356ceddaad42911f188212e4c032120
amitsinha07 <aamitsinha456@gmail.com>
2026-02-23 18:57:12 +0530

    fix(TRV13/2.0.0): set providers[0].time.timestamp to current time in on_search_1 and on_search_3

--- src/config/mock-config/TRV13/2.0.0/on_search/on_search_1/generator.ts
+++ src/config/mock-config/TRV13/2.0.0/on_search/on_search_1/generator.ts
@@ -1,7 +1,11 @@
 export async function onSearchIncrementalPull1Generator(existingPayload: any, sessionData: any) {
+  const currentTimestamp = new Date().toISOString();
 
   existingPayload?.message?.catalog?.providers?.map((provider: any) => {
     provider.descriptor.code = sessionData?.search_1_descriptor_code ?? "HOTEL";
-  })
+    if (provider.time) {
+      provider.time.timestamp = currentTimestamp;
+    }
+  });
   return existingPayload;
 } 
