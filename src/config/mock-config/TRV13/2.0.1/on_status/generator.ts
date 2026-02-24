91cccb7a3cec9d7f90e3ee3c09a6ebc5e772e7fd
sourabhsaini11 <ss1841620@gmail.com>
2026-02-24 13:44:38 +0530

    fix:use flat for nested array

--- src/config/mock-config/TRV13/2.0.1/on_status/generator.ts
+++ src/config/mock-config/TRV13/2.0.1/on_status/generator.ts
@@ -4,7 +4,7 @@ export async function onStatusDefaultGenerator(
 ) {
 
   const payments =
-    sessionData?.on_confirm_payments?.[0]?.map((item: any) => {
+    sessionData?.on_confirm_payments?.flat()?.map((item: any) => {
       if (item.type === "ON-FULFILLMENT") {
         return {
           ...item,
@@ -28,18 +28,18 @@ export async function onStatusDefaultGenerator(
 
   existingPayload.message.order.provider.id =
     sessionData?.confirm_provider_id ?? "P1";
-  existingPayload.message.order.items = sessionData?.confirm_items[0] ?? [];
+  existingPayload.message.order.items = sessionData?.confirm_items?.flat() ?? [];
   existingPayload.message.order.quote = sessionData?.confirm_quote ?? {};
   existingPayload.message.order.billing = sessionData?.confirm_billing ?? {};
   existingPayload.message.order.fulfillments =
-    sessionData?.confirm_fulfillments[0] ?? [];
-  existingPayload.message.order.tags = sessionData?.confirm_tags[0] ?? [];
+    sessionData?.confirm_fulfillments?.flat() ?? [];
+  existingPayload.message.order.tags = sessionData?.confirm_tags?.flat() ?? [];
   existingPayload.message.order.cancellation_terms =
-    sessionData?.on_select_cancellation_terms[0] ?? [];
+    sessionData?.on_select_cancellation_terms?.flat() ?? [];
   existingPayload.message.order.provider.tags =
-    sessionData?.on_select_provider_tags[0] ?? [];
+    sessionData?.on_select_provider_tags?.flat() ?? [];
   existingPayload.message.order.items[0].tags =
-    sessionData?.on_select_item_tags[0] ?? [];
+    sessionData?.on_select_item_tags?.flat() ?? [];
   existingPayload.message.order.updated_at =
     sessionData?.context?.timestamp ?? new Date().toISOString();
   return existingPayload;
