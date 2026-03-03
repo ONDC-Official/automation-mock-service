418dded0cd2730d5d8ae5a8dd4b4bf9b76991f8f
Sharique <sharique@WILASSETA0046.local>
2026-03-03 15:14:14 +0530

    fix: order id issue in cancel

--- src/config/mock-config/TRV13/2.0.0/cancel/generator.ts
+++ src/config/mock-config/TRV13/2.0.0/cancel/generator.ts
@@ -2,6 +2,6 @@ export async function cancelDefaultGenerator(
   existingPayload: any,
   sessionData: any
 ) {
-  // existingPayload.message.order_id = sessionData?.on_confirm_orderID ?? "01";
+  existingPayload.message.order_id = sessionData?.on_confirm_orderID ?? "01";
   return existingPayload;
 }
