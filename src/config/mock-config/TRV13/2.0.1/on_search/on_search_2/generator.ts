3c74ae7e1b5e2cf9a7c8b5cfa11912a54368b750
amitsinha07 <aamitsinha456@gmail.com>
2026-03-05 15:05:42 +0530

    fix: on_search emitting issue fixed

--- src/config/mock-config/TRV13/2.0.1/on_search/on_search_2/generator.ts
+++ src/config/mock-config/TRV13/2.0.1/on_search/on_search_2/generator.ts
@@ -500,13 +500,16 @@ export async function onSearch_2_Generator(
       ],
     },
   ];
-  existingPayload.message.catalog.tags = sessionData?.on_search_1_tags[0] ?? [];
-  existingPayload.message.catalog.tags[0].list.push({
-    descriptor: {
-      code: "CURRENT_PAGE_NUMBER",
-    },
-    value: "1",
-  });
+
+  if (existingPayload.message.catalog.tags[0]?.list) {
+    console.log("on_search_2_tags_list", existingPayload.message.catalog.tags[0]);
+    existingPayload.message.catalog.tags[0]?.list.push({
+      descriptor: {
+        code: "CURRENT_PAGE_NUMBER",
+      },
+      value: "1",
+    });
+  }
 
   // Use lodash cloneDeep (better than JSON.parse(JSON.stringify))
   // existingPayload.message.catalog = sessionData?.on_search_1_catalog
