7f4719b3bcb1bbd95e9f1435e831e13f62feebd7
Sharique <shariquezafar111@gmai.com>
2026-03-23 13:23:46 +0530

    feat: added unsoliciated on_status call in trv11 2.0.0 in dev

--- src/config/mock-config/TRV11/BUS/2.0.0/api-factory.ts
+++ src/config/mock-config/TRV11/BUS/2.0.0/api-factory.ts
@@ -55,6 +55,8 @@ export async function Generator(
 			return await onConfirmGenerator(existingPayload, sessionData);
 		case "on_status_BUS_200":
 			return await onStatusActiveGenerator(existingPayload, sessionData);
+		case "unsoliciated_on_status_BUS_200":
+			return await onStatusActiveGenerator(existingPayload, sessionData);	
 		case "on_status_active_BUS_200":
 			return await onStatusActiveGenerator(existingPayload, sessionData);
 		case "on_confirm_delayed_BUS_200":
