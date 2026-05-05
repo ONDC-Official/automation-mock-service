65183562183e28c159115a96d15055b8ce604351
Sahil Sharma <sahil-ondc@users.noreply.github.com>
2026-05-05 16:36:28 +0530

    fix: sending bpp_id by sessionData

--- src/services/data-services.ts
+++ src/services/data-services.ts
@@ -93,7 +93,8 @@ export async function loadMockSessionData(
 		const raw = defaultSessionData;
 		sessionData = raw.session_data;
 		sessionData.transaction_id = transactionID;
-		sessionData.bpp_id = sessionData.bap_id = "staging-automation.ondc.org";
+		sessionData.bpp_id = sessionData.bpp_id ?? "staging-automation.ondc.org";
+		sessionData.bap_id = "staging-automation.ondc.org";
 		sessionData.bap_uri = "https://dev-automation.ondc.org/buyer";
 		sessionData.bpp_uri = "https://dev-automation.ondc.org/seller";
 		sessionData.subscriber_url = subscriber_url;
