e8ed1e726741e4f5ff1a8e0deadc52160a6082ff
sourabhsaini11 <68590150+sourabhsaini11@users.noreply.github.com>
2026-06-08 15:19:40 +0530

    Add descriptor to order items mapping

--- src/config/mock-config/NIC2004:60232/LOGISTICS/1.2.5/update/generator.ts
+++ src/config/mock-config/NIC2004:60232/LOGISTICS/1.2.5/update/generator.ts
@@ -11,7 +11,7 @@ export async function updateGenerator(
   
   existingPayload.message.order.id = sessionData.order_id;
 
-  existingPayload.message.order.items = sessionData.items.map((item: { id: any; category_id: any; }) => ({
+  existingPayload.message.order.items = sessionData.items.map((item: { id: any; category_id: any; descriptor:any }) => ({
     id: item.id,
     category_id: item.category_id,
     descriptor: item.descriptor
