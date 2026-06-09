4fa2fb16e86a9d659dbb4e0697ee262125b76f22
Sharique <shariquezafar111@gmai.com>
2026-06-09 16:34:58 +0530

    fix: bpp_id,uri,bap_id,bap_uri and city code issue in TRV13-2.0.1 in stage

--- src/config/mock-config/TRV13/2.0.1/on_init/generator.ts
+++ src/config/mock-config/TRV13/2.0.1/on_init/generator.ts
@@ -2,6 +2,15 @@ export async function onInitDefaultGenerator(
   existingPayload: any,
   sessionData: any,
 ) {
+  if (existingPayload.context) {
+    existingPayload.context.bap_id = sessionData?.bap_id;
+    existingPayload.context.bap_uri = sessionData?.bap_uri;
+    existingPayload.context.bpp_id = sessionData?.bpp_id;
+    existingPayload.context.bpp_uri = sessionData?.bpp_uri;
+    if (existingPayload.context.location?.city) {
+      existingPayload.context.location.city.code = sessionData?.city_code;
+    }
+  }
   const payments =
     sessionData?.init_payments?.flat().map((payment: any) => {
       if (payment?.type === "PRE-ORDER") {
--- src/config/mock-config/TRV13/2.0.1/on_status/generator.ts
+++ src/config/mock-config/TRV13/2.0.1/on_status/generator.ts
@@ -3,6 +3,15 @@ export async function onStatusDefaultGenerator(
   sessionData: any,
   isCityCode?: boolean
 ) {
+  if (existingPayload.context) {
+    existingPayload.context.bap_id = sessionData?.bap_id;
+    existingPayload.context.bap_uri = sessionData?.bap_uri;
+    existingPayload.context.bpp_id = sessionData?.bpp_id;
+    existingPayload.context.bpp_uri = sessionData?.bpp_uri;
+    if (existingPayload.context.location?.city) {
+      existingPayload.context.location.city.code = sessionData?.city_code;
+    }
+  }
 
   const payments =
     sessionData?.on_confirm_payments?.flat()?.map((item: any) => {
